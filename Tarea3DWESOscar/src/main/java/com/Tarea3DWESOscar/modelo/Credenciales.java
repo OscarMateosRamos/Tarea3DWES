2902ce1437b8142cb2ec3d69ab4478788e9b4632
OscarMateosRamos <dsi85944@educastur.es>
2024-11-28 12:56:25 +0100

    Clase Credenciales creado

---
+++ Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/modelo/Credenciales.java
@@ -0,0 +1,91 @@
+package com.Tarea3DWESOscar.modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "credenciales")
+public class Credenciales implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "usuario", unique = true)
+	private String usuario;
+
+	@Column(name = "password")
+	private String mensaje;
+
+	public Credenciales() {
+
+	}
+
+	public Credenciales(long id, String usuario, String mensaje) {
+		this.id = id;
+		this.usuario = usuario;
+		this.mensaje = mensaje;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getMensaje() {
+		return mensaje;
+	}
+
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, mensaje, usuario);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Credenciales other = (Credenciales) obj;
+		return id == other.id && Objects.equals(mensaje, other.mensaje) && Objects.equals(usuario, other.usuario);
+	}
+
+	@Override
+	public String toString() {
+		return "Credenciales [id=" + id + ", usuario=" + usuario + ", mensaje=" + mensaje + "]";
+	}
+	
+	
+
+}
