38ddb7963bfb66a7d9229db5993775ad441845e5
OscarMateosRamos <dsi85944@educastur.es>
2024-12-03 14:09:54 +0100

    Cambios realizados en la fachada del invitado

---
+++ Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/vista/ViveroFachadaInvitado.java
@@ -0,0 +1,53 @@
+package com.Tarea3DWESOscar.vista;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.Tarea3DWESOscar.modelo.Planta;
+import com.Tarea3DWESOscar.servicios.ServiciosPlanta;
+
+@Component
+public class ViveroFachadaInvitado {
+	@Autowired
+	ServiciosPlanta servPlanta;
+
+	public void mostrarMenuPrincipal() {
+		int opcion = 0;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("--Menu Invitado--:");
+			System.out.println("1.  Ver plantas.");
+			System.out.println("2.  Salir.");
+
+			try {
+				opcion = sc.nextInt();
+				if (opcion < 1 || opcion > 2) {
+					System.out.println("Opción incorrecta.");
+					continue;
+				}
+				switch (opcion) {
+				case 1:
+					Planta pl = new Planta();
+					servPlanta.verPlanta(pl);
+					break;
+
+				case 2:
+					System.out.println("Salida del menu del menu invitado...");
+					System.out.println("");
+					break;
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Introduce un numero.... Repita el proceso");
+				sc.nextLine();
+				opcion = 0;
+			}
+
+		} while (opcion != 2);
+
+	}
+
+}
