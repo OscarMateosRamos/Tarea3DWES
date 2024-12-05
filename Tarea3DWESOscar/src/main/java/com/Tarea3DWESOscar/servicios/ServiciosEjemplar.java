a63b3a82c9254ff1d6ad24df8a8ff56278861383
OscarMateosRamos <dsi85944@educastur.es>
2024-12-05 12:56:29 +0100

    Servicios del ejemplar modificados

--- Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/servicios/ServiciosEjemplar.java
+++ Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/servicios/ServiciosEjemplar.java
@@ -3,16 +3,27 @@ package com.Tarea3DWESOscar.servicios;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Tarea3DWESOscar.modelo.Ejemplar;
+import com.Tarea3DWESOscar.modelo.Planta;
 import com.Tarea3DWESOscar.repositories.EjemplarRepository;
 import com.Tarea3DWESOscar.repositories.PlantaRepository;
 
-//@Service
+@Service
 public class ServiciosEjemplar {
 
-//	@Autowired
-//	PlantaRepository plantarepo;
-//	
-//	@Autowired
-//	EjemplarRepository ejemplarrepo;
+	@Autowired
+	PlantaRepository plantarepo;
+	@Autowired
+	EjemplarRepository ejemplarrepo;
+
+	public void insertarEjemplar(Ejemplar ej) {
+		ejemplarrepo.saveAndFlush(ej);
+	}
+
+	public void verEjemplar(Ejemplar ej) {
+		ejemplarrepo.saveAndFlush(ej);
+	}
+	
+	
 	
 }
