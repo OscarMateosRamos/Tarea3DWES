49bc40af7efda8c3c7fa00b4a7e3207b8b111f21
Osmara <dsi85944@educastur.es>
2024-12-07 18:18:33 +0100

    Cambios realizados en servicios Planta

--- Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/servicios/ServiciosPlanta.java
+++ Tarea3DWESOscar/src/main/java/com/Tarea3DWESOscar/servicios/ServiciosPlanta.java
@@ -1,10 +1,11 @@
 package com.Tarea3DWESOscar.servicios;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.Tarea3DWESOscar.modelo.Planta;
-import com.Tarea3DWESOscar.repositories.EjemplarRepository;
 import com.Tarea3DWESOscar.repositories.PlantaRepository;
 
 @Service
@@ -13,22 +14,18 @@ public class ServiciosPlanta {
 	@Autowired
 	PlantaRepository plantarepo;
 
-//	@Autowired
-//	EjemplarRepository ejemplarrepo;
-
 	public boolean validarPlanta(Planta pl) {
 
 		if (pl.getCodigo().isEmpty()) {
-
 			return false;
 		}
 
 		if (pl.getCodigo().length() < 3 || pl.getCodigo().length() > 20) {
 			return false;
 		}
-
-		if (plantarepo.findById(pl)) {
-			System.out.println("Ya existe el codigo");
+		
+		if (plantarepo.findByCodigo(pl.getCodigo()) != null) {
+			System.out.println("El codigo ya existe....");
 			return false;
 		}
 
@@ -47,15 +44,22 @@ public class ServiciosPlanta {
 	public void insertarPlanta(Planta pl) {
 		plantarepo.saveAndFlush(pl);
 	}
-	
-	
+
 	public void modificarPlanta(Planta pl) {
 		plantarepo.saveAndFlush(pl);
 	}
-	
-	public void verPlanta(Planta pl) {
-		plantarepo.findAll();
+
+	public void vertodasPlantas() {
+		List<Planta> plantas = plantarepo.findAll();
+		System.out.println("----LISTADO DE PLANTAS-----");
+		for (Planta p : plantas) {
+			
+			System.out.println(p);
+			
+		}
+		System.out.println("----------------------------");
+//		for (Planta p : plantas) {
+//			System.out.println(p);
+//		}
 	}
-	
-	
 }
